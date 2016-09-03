70083ff1bc0499034323d00fbbfab21e42025cef
Bethany Morin <morinbe@gmail.com>
2016-09-03 07:02:21 -0400

    Add verification of base JSON search result structure to test

--- who_data/tests/search_views.py
+++ who_data/tests/search_views.py
@@ -4,4 +4,16 @@ from who_data.tests import IntegrationTestBase
 class SearchViewTests(IntegrationTestBase):
 
     def test_json_structure(self):
-        self.TestApp.get('/search/ping', status=200)
+        '''
+        Test that the basic search JSON logic works and has expected structure
+        '''
+        page = self.TestApp.get('/search/ping', status=200)
+        self.assertTrue('meta' in page.json)
+        self.assertTrue('self_link' in page.json['meta'])
+        self.assertTrue('next_link' in page.json['meta'])
+        self.assertTrue('prev_link' in page.json['meta'])
+        self.assertTrue('results' in page.json)
+        self.assertTrue('total_items' in page.json['results'])
+        self.assertTrue('total_pages' in page.json['results'])
+        self.assertTrue('page' in page.json['results'])
+        self.assertTrue('items' in page.json['results'])
